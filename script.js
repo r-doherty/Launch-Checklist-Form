3b4eac2b753e30857d5eeb57f1c6241c942b5cfb
Ryan Doherty <rjd.atx@gmail.com>
2020-09-06 17:22:43 -0500

    re-design + facelift

--- script.js
+++ script.js
@@ -12,74 +12,83 @@
 <img src="${}">
 */
 
-window.addEventListener("load", function() {
-   let form = document.querySelector("form")
-   
-   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
-      response.json().then(function(json) {
-         const missionTarget = document.querySelector("#missionTarget");
-         let missionSelecter = (Math.floor(Math.random()*json.length));
-         missionTarget.innerHTML =
-         `
+window.addEventListener('load', function () {
+  let form = document.querySelector('form');
+
+  fetch('https://handlers.education.launchcode.org/static/planets.json').then(
+    function (response) {
+      response.json().then(function (json) {
+        const missionTarget = document.querySelector('#missionTarget');
+        let missionSelecter = Math.floor(Math.random() * json.length);
+        missionTarget.innerHTML = `
          <h2>Mission Destination</h2>
-         <ol>
-            <li>Name: ${json[missionSelecter].name}</li>
-            <li>Diameter: ${json[missionSelecter].diameter}</li>
-            <li>Star: ${json[missionSelecter].star}</li>
-            <li>Distance from Earth: ${json[missionSelecter].distance}</li>
-            <li>Number of Moons: ${json[missionSelecter].moons}</li>
-         </ol>
-         <img src="${json[missionSelecter].image}">
-         `
+            <img src="${json[missionSelecter].image}">
+            <p>${json[missionSelecter].name}</p>
+            <p>${json[missionSelecter].star} System</p>
+            <p>Diameter: ${json[missionSelecter].diameter} </p>
+            <p> ${json[missionSelecter].distance}</p>
+            <p>${json[missionSelecter].moons} moons</p>
+         
+         `;
       });
-   });
-           
-   form.addEventListener("submit", function(event) {
+    }
+  );
 
-      let pilotNameInput = document.querySelector("input[name=pilotName]");
-      let copilotNameInput = document.querySelector("input[name=copilotName]");
-      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
-      let cargoMassInput = document.querySelector("input[name=cargoMass]");
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let pilotNameInput = document.querySelector('input[name=pilotName]');
+    let copilotNameInput = document.querySelector('input[name=copilotName]');
+    let fuelLevelInput = document.querySelector('input[name=fuelLevel]');
+    let cargoMassInput = document.querySelector('input[name=cargoMass]');
 
-      pilotStatus.innerHTML =`${pilotNameInput.value}`;
-      copilotStatus.innerHTML = `${copilotNameInput.value}`;
-      fuelStatus.innerHTML = "Fuel level high enough for launch";
-      cargoStatus.innerHTML = "Cargo mass low enough for launch";
+    pilotStatus.innerHTML = `Pilot: ${pilotNameInput.value}`;
+    copilotStatus.innerHTML = `Co-Pilot: ${copilotNameInput.value}`;
+    fuelStatus.innerHTML = 'Fuel: Sufficient for Orbit';
+    cargoStatus.innerHTML = 'Cargo Mass: Within Parameters';
 
-      if(pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "")
-      {
-         alert("All fields are required!");
-         event.preventDefault();
+    if (
+      pilotNameInput.value === '' ||
+      copilotNameInput.value === '' ||
+      fuelLevelInput.value === '' ||
+      cargoMassInput.value === ''
+    ) {
+      alert('All fields are required!');
+    } else if (
+      !isNaN(pilotNameInput.value) ||
+      !isNaN(copilotNameInput.value) ||
+      isNaN(fuelLevelInput.value) ||
+      isNaN(cargoMassInput.value)
+    ) {
+      alert(
+        'Invalid Entry! Please ensure that valid information has been entered for each field.'
+      );
+    } else {
+      faultyItems.style.visibility = 'visible';
+      if (fuelLevelInput.value < 10000) {
+        launchStatus.innerHTML = 'LAUNCH ERROR';
+        launchStatus.style.color = 'red';
+        fuelStatus.innerHTML = 'Fuel: Insufficent for Orbit';
+        fuelStatus.style.color = 'red';
+      } else {
+        fuelStatus.innerHTML = 'Fuel: Sufficent for Orbit';
+        fuelStatus.style.color = 'green';
       }
-      else if(!isNaN(pilotNameInput.value) || !isNaN(copilotNameInput.value) || isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value))
-      {
-         alert("Invalid Entry! Please ensure that valid information has been entered for each field.");
-         event.preventDefault();
-      }
-      else if(fuelLevelInput.value < 10000)
-      {
-         launchStatus.innerHTML = "Shuttle not ready for launch";
-         launchStatus.style.color = "red";
-         faultyItems.style.visibility = "visible";
-         fuelStatus.innerHTML = "Fuel level too low for launch";
-         event.preventDefault();
-      }
-      else if(cargoMassInput.value > 10000)
-      {
-         launchStatus.innerHTML = "Shuttle not ready for launch";
-         launchStatus.style.color = "red";
-         faultyItems.style.visibility = "visible";
-         cargoStatus.innerHTML = "Cargo mass too high to launch";
-         event.preventDefault();
-      }
-      else
-      {
-         launchStatus.innerHTML = "Shuttle is ready for launch";
-         launchStatus.style.color = "green";
-         faultyItems.style.visibility = "visible";
-         event.preventDefault();
-      }
-      
 
-   });
+      if (cargoMassInput.value > 10000) {
+        launchStatus.innerHTML = 'LAUNCH ERROR';
+        launchStatus.style.color = 'red';
+        cargoStatus.innerHTML = 'Cargo Mass: Exceeds Limit';
+        cargoStatus.style.color = 'red';
+      } else {
+        cargoStatus.innerHTML = 'Cargo Mass: Within Parameters';
+        cargoStatus.style.color = 'green';
+      }
+
+      if (cargoMassInput.value < 10000 && fuelLevelInput.value > 10000) {
+        launchStatus.innerHTML = 'READY FOR LAUNCH';
+        launchStatus.style.color = 'green';
+        event.preventDefault();
+      }
+    }
+  });
 });
